1ae88f17a3f0ef3e2311b3868ea39d9fbb84728c
Adria Priego Garcia <apriego-@car12s1.42barcelona.com>
2023-08-17 16:58:46 +0200

    Testing branches

--- minishell.c
+++ minishell.c
@@ -6,14 +6,35 @@
 /*   By: apriego- <apriego-@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/14 13:16:48 by apriego-          #+#    #+#             */
-/*   Updated: 2023/08/17 13:00:49 by apriego-         ###   ########.fr       */
+/*   Updated: 2023/08/17 16:57:22 by apriego-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+int	ft_array_len(char **array)
+{
+	int	i;
+
+	i = 0;
+	while (array[i])
+		i++;
+	return (i);
+}
+
 int	main(void)
 {
-	char *const str[3] = {"ls", "-l", NULL};
-	execve("/bin/ls", str, NULL);
+	char	*entry;
+	char	**entry_split;
+	int		i;
+
+	entry = getcwd(NULL, 0);
+	entry_split = ft_split(entry, '/');
+	i = ft_array_len(entry_split);
+	ft_printf("%s$>", entry_split[i - 1]);
+	while (1)
+	{
+		
+	}
+	return(0);
 }
